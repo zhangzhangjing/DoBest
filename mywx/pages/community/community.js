58681ff0213b019a2c584f2284eae2d56c0ca60e
jing2018 <2370401878@qq.com>
2018-05-21 14:39:05 +0800

    上拉加载和下拉刷新

--- mywx/pages/community/community.js
+++ mywx/pages/community/community.js
@@ -7,6 +7,7 @@ Page({
     _num: 1,
     imgUrl:'../../images/zan.png',
     opacity: 1,
+    isHideLoadMore:false,
     communityList: [{
       id: 0,
       username:"重庆新闻网",
@@ -59,6 +60,16 @@ Page({
         createtime: "今天 10：28",
         zan: 1,
         pictures: ['compic1.jpg' , 'compic6.jpg', 'compic4.jpg']
+      },
+      {
+        id: 6,
+        username: "重庆新闻网",
+        userPic: 'compic2.jpg',
+        content: "多少红颜悴，多少相思碎，唯留血染墨香哭乱冢。 ",
+        createtime: "今天 14：28",
+        zan: 0,
+        _src: false,
+        pictures: ['compic1.jpg', 'compic2.jpg', 'compic3.jpg'],
       }]
   },
   
@@ -82,5 +93,73 @@ Page({
       }   
     }
  
+  },
+  // onPullDownRefresh: function () {
+  //   wx.stopPullDownRefresh()
+  // },
+  //下拉刷新
+  onPullDownRefresh: function () {
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+    //模拟加载
+    setTimeout(function () {
+      // complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+    }, 1500);
+  },
+  //加载更多
+  onReachBottom: function () {
+    console.log('加载更多')
+    setTimeout(() => {
+      this.setData({
+        isHideLoadMore: true,
+        communityList: [
+          {
+            id: 7,
+            username: "Angular",
+            userPic: 'compic1.jpg',
+            content: "苍茫大地一剑尽挽破，何处繁华笙歌落。斜倚云端千壶掩寂寞，纵使他人空笑我。",
+            createtime: "2018-5-8 12：23",
+            zan: 1,
+            pictures: ['compic5.jpg']
+          }, {
+            id: 8,
+            username: "周星驰",
+            userPic: 'compic3.jpg',
+            content: " 寄君一曲，不问曲终人聚散。",
+            createtime: "今天 14：28",
+            zan: 1,
+            pictures: ['compic1.jpg', 'compic5.jpg']
+          },
+          {
+            id: 9,
+            username: "张兴义",
+            userPic: 'compic6.jpg',
+            content: "灯火星星，人声杳杳，歌不尽乱世烽火。",
+            createtime: "2018-4-9 12：23",
+            zan: 0,
+            pictures: ['compic1.jpg', 'compic2.jpg', 'compic3.jpg']
+          },
+          {
+            id: 10,
+            username: "大圣",
+            userPic: 'compic5.jpg',
+            content: "一年老一年，一日没一日，一秋又一秋，一辈催一辈 一聚一离别，一喜一伤悲，一榻一身卧，一生一梦里 寻一夥相识，他一会咱一会 那一般相知，吹一会唱一会。",
+            createtime: "2018-4-9 12：23",
+            zan: 0,
+            pictures: ['compic1.jpg', 'compic3.jpg']
+          },
+          {
+            id: 11,
+            username: "四月梅花",
+            userPic: 'compic6.jpg',
+            content: "[◆．” 莪菂丗堺,.﹏;*.只剩﹌.棒棒糖* .？ ┽☆ρs﹎..偶是↘ㄚi个贪玩旳╭ →孩孑?_? わ ↘。",
+            createtime: "今天 10：28",
+            zan: 1,
+            pictures: ['compic1.jpg', 'compic6.jpg', 'compic4.jpg']
+          }
+        ],
+      })
+    }, 1000)
   }
 })
--- mywx/pages/person/person.js
+++ mywx/pages/person/person.js
@@ -5,43 +5,39 @@ const app = getApp()
 Page({
   data: {
     userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    myInfo: [{
-      id: 0,
-      username: "半梦半醒",
-      userPic: 'compic2.jpg',
-      content: "多少红颜悴，多少相思碎，唯留血染墨香哭乱冢。 ",
-    }]
+    nickName:'',
+    avartar:'',
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    let that = this;
+    wx.getUserInfo({
+      success: function (res) {
+        var myInfo = res.userInfo;
+        that.nickName = myInfo.nickName;
+        that.avartar = myInfo.avatarUrl;
+        that.setData({
+          nickName: that.nickName,
+          avartar: that.avartar
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-    console.log(this.userInfo)
+    })
   },
+  changePic:function(e){
+    let that = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        that.setData({
+          avartar: tempFilePaths
+        })
+      }
+    })
+
+
+  }
   
 })
