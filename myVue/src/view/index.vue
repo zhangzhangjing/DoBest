34acbed5528822b08cc88b11827d67e1a4f432bb
jing2018 <2370401878@qq.com>
2018-06-20 22:44:33 +0800

    数据

--- myVue/src/view/index.vue
+++ myVue/src/view/index.vue
@@ -4,7 +4,17 @@
 
 <script>
     export default {
-        name: "index"
+        name: "index",
+        product: {
+            name: "mac book pro",
+            type: [
+                {year:"2016版", memery: "8G", cpu:"i5", price:12000},
+                {year:"2016版", memery: "4G", cpu:"i5",price:10000},
+                {year:"2017版", memery: "8G", cpu:"i5",price:14000},
+                {year:"2017版", memery: "8G", cpu:"i7",price:16000},
+                {year:"2017版", memery: "16G", cpu:"i7",price:18000},
+            ]
+        },
     }
 </script>
 
