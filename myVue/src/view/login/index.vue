fb58bd5eb178740052e00fb7a21cf940ffa87778
jing2018 <2370401878@qq.com>
2018-07-16 09:39:27 +0800

    登录

--- myVue/src/view/login/index.vue
+++ myVue/src/view/login/index.vue
@@ -2,18 +2,52 @@
   <div>
     <main-header message="" returnbtn="true"></main-header>
     <el-form label-position="left" label-width="0px"  fill="#ffa200" class="demo-ruleForm login-container">
-      <h3 class="title">登录</h3>
-      <el-form-item prop="phone">
-        <el-input type="text" v-model="phone" auto-complete="off" placeholder="手机号"></el-input>
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-input type="password" v-model="password" auto-complete="off" placeholder="密码"></el-input>
-      </el-form-item>
-      <el-checkbox v-model="checked"  fill="#ffa200" class="remember">记住密码</el-checkbox>
-      <el-button type="text" @click="resetBtnTapped" style="float:right;">忘记密码?</el-button>
-      <el-form-item style="width:100%;">
-        <el-button type="primary" style="width:100%;" @click="handleSubmit2">登录</el-button>
-      </el-form-item>
+      <div class="contentre">
+        <div class="u-input m_t_30">
+          <label>帐号</label>
+          <div class="inputWrapper">
+            <input type="text" v-model="phone" placeholder="请输入手机号码或邮箱" class="inputme"/>
+          </div>
+          <span class="msg z-err">
+            <span v-show="phone">
+              <i class="el-icon-circle-close"></i><font>请输入帐号</font>
+            </span>
+					</span>
+        </div>
+
+        <div class="u-input" style="margin-bottom: 10px;">
+          <label>密码</label>
+          <div class="inputWrapper">
+            <input type="password" v-model="password" placeholder="4-20个字符，区分大小写" class="inputme"/>
+          </div>
+          <span class="msg z-err">
+            <span v-show="password">
+						  <i class="el-icon-circle-close"></i><font>请输入密码</font>
+            </span>
+					</span>
+        </div>
+
+
+
+        <div class="u-input" style="margin-bottom: 0;margin-top: 0">
+          <label></label>
+          <div class="inputWrapper b_b_n">
+            <el-checkbox v-model="checked"  fill="#ffa200" class="remember">记住密码</el-checkbox>
+          </div>
+          <span class="msg z-err" style="display: inline-block;">
+					</span>
+        </div>
+
+        <div class="u-input">
+          <label></label>
+          <div class="inputWrapper b_b_n">
+            <el-button type="primary" style="width:100%;" @click="handleSubmit2">登录</el-button>
+          </div>
+          <span class="msg z-err" style="display: inline-block;">
+					</span>
+        </div>
+
+      </div>
     </el-form>
   </div>
 </template>
@@ -26,8 +60,9 @@
       return {
         msg: 'MyPath的页面',
         checked: true,
-        phone: '18791589763',
-        password: '123456'
+        phone: '',
+        password: '',
+        vcode:''
       }
     },
     components: {
@@ -65,7 +100,7 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="stylus">
   h1, h2 {
     font-weight: normal;
   }
@@ -81,15 +116,19 @@
     color: #42b983;
   }
   .login-container {
+    border-top: 5px solid #ffa200;
     border-radius: 5px;
     -moz-border-radius: 5px;
     background-clip: padding-box;
     margin-bottom: 20px;
     background-color: #f9fafc;
     margin: 180px auto;
-    width: 350px;
+    width: 500px;
     padding: 35px 35px 15px;
+    box-shadow: 0 0 9px 0 rgba(92,128,255,0.17);
   }
+  .contentre
+    width 500px
   .remember {
     margin: 0 0 35px;
   }
@@ -100,4 +139,56 @@
     white-space: nowrap;
     float: left;
   }
+  .u-input
+    position: relative;
+    width: 100%;
+    margin-bottom 45px
+  .vcode
+    position absolute
+    width 50%
+    right 0
+    height 50px
+    background #f9fafc
+    bottom -2px
+  .btnvcode
+    border 1px solid #ffa200
+    height: 30px;
+    padding: 5px 15px;
+    border-radius: 3px;
+    background: #fff;
+    vertical-align: middle;
+    margin-top: 10px;
+    cursor pointer
+    outline none
+    color #ffa200
+  .u-input .b_b_n
+    border-bottom none
+  .u-input label
+    width: calc(20% - 15px);
+    display: inline-block;
+    text-align: right;
+    font-size: 16px;
+    padding-right 15px
+    color: #738299;
+    font-family: "微软雅黑";
+    margin-bottom: 4px;
+  .u-input .msg
+    width: calc(22% - 10px);
+    position: relative;
+    text-align left
+    margin-left 10px
+    font-size 12px
+    color red
+    display inline-block
+  .inputWrapper
+    width: 56%;
+    height 30px
+    position: relative;
+    display inline-block
+    border-bottom: 1px solid #ddd;
+  .inputme
+    border none
+    outline none
+    width 90%
+    background none
 </style>
